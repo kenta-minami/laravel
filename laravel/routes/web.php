f61046a8e0921ca64cfbf294cfef38d057c4979c
kenta-minami <k.minami@teradox.net>
2022-12-23 17:34:22 +0900

    display of data

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list', [TodoListController::class, 'index']);
